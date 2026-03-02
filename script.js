0ed2a1ad1be9261365b2c13277e3d19a49ee4376
nitro <108837401+nitrogeo@users.noreply.github.com>
2026-03-02 09:49:33 -0600

    fix svg + js typing anim WIP idk

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var i = 0;
 var txt = 'Lorem ipsum typing effect!';
 var speed = 50;
-
+/*
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("typeWriter").innerHTML += txt.charAt(i);
@@ -10,3 +10,12 @@ function typeWriter() {
   }
 }
 typeWriter();
+*/
+
+var typed = new Typed('#typeWriter', {
+  strings: ["Developer.", "Designer.", "Problem Solver."],
+  typeSpeed: 75,
+  backSpeed: 50,
+  loop: true
+});
+
